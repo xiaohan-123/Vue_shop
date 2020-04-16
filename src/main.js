bea652e7951f3cfea9dbf2550f86440733d2bada
xiaohan <15615753615@163.com>
2020-04-16 13:14:59 +0800

    '4.16-商品添加，优化复选框的样式'

--- src/main.js
+++ src/main.js
@@ -25,6 +25,17 @@ Vue.config.productionTip = false
 Vue.use(Element)
 Vue.component('tree-table', TreeTable)
 
+Vue.filter('dateFormat', function (origiVal) {
+  const dt = new Date(origiVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
