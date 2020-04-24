df4ca8119a24b8f1451f385c1f0e6dcd751481e7
xiaohan <15615753615@163.com>
2020-04-24 15:18:17 +0800

    4.24_完成商品添加，下一次订单列表

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 导入全局样式表
 import './assets/css/global.css'
+// 导入全局样式表
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 // 导入字体图标
 import './assets/fonts/icon.css'
 import axios from 'axios'
@@ -24,6 +30,8 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(Element)
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 Vue.filter('dateFormat', function (origiVal) {
   const dt = new Date(origiVal)
