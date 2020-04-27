b06693bb6909c0189effa6bd5697f42d40505ca3
xiaohan <15615753615@163.com>
2020-04-27 16:19:26 +0800

    4.27完成订单列表

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/add.vue'
-
+import Order from '../components/order/order.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -28,7 +28,8 @@ const router = new Router({
         { path: '/categories', component: Cate },
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList },
-        { path: '/goods/add', component: Add }
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order }
       ]
     }
   ]
