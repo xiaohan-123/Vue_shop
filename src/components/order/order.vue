e86bd438e1dae130f3598979bdde134b2d44803f
xiaohan <15615753615@163.com>
2020-04-27 20:02:50 +0800

    4.27完成数据统计

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -10,8 +10,9 @@
     <el-card>
       <el-row>
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query"
+                    clearable @clear="getOrderList">
+            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
           </el-input>
         </el-col>
       </el-row>
